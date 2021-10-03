9a634e600636b6420e182c4a1e81707aeea3696f
Nolan <nem5345@rit.edu>
2021-10-03 13:49:06 -0400

    I think I finished, needs testing still

--- fitness.py
+++ fitness.py
@@ -9,14 +9,9 @@ def main():
     BMR_n = 0
     BMR_n = BMR(info[0], info[1], info[2])
     BMR_n = BMR_n * 1.2
-    calorie_loss = weight_loss(info[3])
-    
-    #TODO Implement workout information into information collected
-
-    # calorie_burned = workout_calories()
-    # calories = BMR - calorie_loss + calorie_burned
-    # string = str(calories) + ' is the amount you should eat to hit your goals'
-    # print(string)
+    calories = BMR_n + info[4] - (info[3] * 500) 
+    string = str(calories) + ' is the amount you should eat to hit your goals'
+    print(string)
     exit()
 
 def info_collection():
@@ -41,7 +36,16 @@ def info_collection():
         if reality_check(fat_goal, weeks) == True:
             i == 1
             break
-    return weight, height, age, fat_goal/weeks
+    # Ask for strength training time and error check it
+    string = 'How many hours of strength training a day?: '
+    calories_strength = check_is_digit(string) * 500
+    # Ask for miles ran and error check it
+    string = 'How many miles do you run per day?: '
+    calories_run = check_is_digit(string) * 125 
+    # Calculate how many calories are burned a day
+    calories_burn =  calories_run + calories_strength
+    # Return all the information 
+    return weight, height, age, fat_goal/weeks, calories_burn
 
 def reality_check(fat_goal, weeks):
     if (fat_goal/weeks) > 2:
@@ -56,18 +60,6 @@ def BMR(weight, height, age):
     BMR = 66 + (6.2 * weight) + (12.7 * height) - (6.67 * age)
     return BMR
 
-def weight_loss(fat_per_wk):
-    cal_loss = fat_per_wk * 500
-    return cal_loss
-
-def workout_calories():
-    string = 'How many hours of strength training a day?: '
-    strength_workout = check_is_digit(string)
-    calories_strength = strength_workout * 500
-    string = 'How many miles do you run per day?: '
-    running_workout = check_is_digit(string)
-    calories_run = running_workout * 125
-    return calories_run + calories_strength
 
 if __name__ == "__main__":
     main()
